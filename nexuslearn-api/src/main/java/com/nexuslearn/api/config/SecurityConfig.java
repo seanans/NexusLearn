99c9fc8f2e46f12b4ec9e6eb3c78bc638adec4d6
seanans <vakelchuk2101@knu.ua>
2026-06-05 00:34:41 +0300

    Fix silent refresh of access token. Accept only angular requests.

--- nexuslearn-api/src/main/java/com/nexuslearn/api/config/SecurityConfig.java
+++ nexuslearn-api/src/main/java/com/nexuslearn/api/config/SecurityConfig.java
@@ -46,13 +46,16 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.cors(cors -> cors.configurationSource(request -> {
             var config = new org.springframework.web.cors.CorsConfiguration();
-            config.setAllowedOriginPatterns(java.util.List.of("*")); // Local testing
-            // config.setAllowedOrigins(java.util.List.of("http://localhost:4200")); // Angular
+            config.setAllowedOrigins(java.util.List.of("http://localhost:4200")); // Angular
             config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(java.util.List.of("*"));
             config.setAllowCredentials(true);
             return config;
-        })).csrf(AbstractHttpConfigurer::disable).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll() // Open login/register endpoints
+        })).csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception
+                .authenticationEntryPoint((request, response, authException) ->
+                        response.sendError(jakarta.servlet.http.HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized")))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll() // Open login/register endpoints
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll() // Open OpenAPI docs
                 .requestMatchers("/ws-chat/**").permitAll() // Open for Websocket handshake
                 .anyRequest().authenticated()).authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
